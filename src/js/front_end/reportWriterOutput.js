501d1c6afd7e7ed88f2771e5394311cdbe26e4d1
Netta Paulson <paulsonnt@vcu.edu>
2021-01-04 19:57:12 -0500

    possible comma fix

--- src/js/front_end/reportWriterOutput.js
+++ src/js/front_end/reportWriterOutput.js
@@ -41,9 +41,9 @@ for (var i = 0; i < rows.length; i++) {
     var row = [], cols = rows[i].querySelectorAll("td, th");
     console.log('cols', cols);
     for (var j = 0; j < cols.length; j++) 
-        console.log("row", row)
+        console.log("row1", row)
         row.push(cols[j].innerText);
-        console.log("row", row)
+        console.log("row2", row)
     csv.push(row.join(","));        
 }
 // Download CSV file
